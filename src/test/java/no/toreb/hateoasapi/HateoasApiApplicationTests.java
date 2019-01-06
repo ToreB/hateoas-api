14c4386a5abda6ccf201698ce3ce3997d05b8bab
Tore Brandtzæg <toreb27@gmail.com>
2019-01-06 11:18:07 +0100

    Connect to H2 database.

--- src/test/java/no/toreb/hateoasapi/HateoasApiApplicationTests.java
+++ src/test/java/no/toreb/hateoasapi/HateoasApiApplicationTests.java
@@ -1,14 +1,18 @@
 package no.toreb.hateoasapi;
 
+import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureTestDatabase
 public class HateoasApiApplicationTests {
 
+	@FlywayTest
 	@Test
 	public void contextLoads() {
 	}
