10ae33e9a95c68e86e97b7cee727bc1ed51c4739
Tore Brandtzæg <toreb27@gmail.com>
2019-01-06 09:06:54 +0100

    Initial commit.

---
+++ src/main/java/no/toreb/hateoasapi/HateoasApiApplication.java
@@ -0,0 +1,14 @@
+package no.toreb.hateoasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasApiApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(HateoasApiApplication.class, args);
+	}
+
+}
+
